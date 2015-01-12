ecc56ff9b63ce7e51a214b7a620d4f0b3dec894b
Tom Sheffler <thomas.sheffler@weemo.com>
2015-01-12 15:28:43 -0800

    fix friends method

--- app/controllers/call_controller.rb
+++ app/controllers/call_controller.rb
@@ -5,4 +5,13 @@ class CallController < ApplicationController
     @friends = User.all.select{ |u| u != @me }
   end
 
+  # whose presence we are interested in
+  def friends
+    @me = current_user
+    @friends = User.all.select{ |u| u != @me }
+
+    # see views/call/friends.json.jbuilder for field selection
+    render :format => :json
+  end
+
 end
