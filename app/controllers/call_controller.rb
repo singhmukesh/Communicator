482b6edb5ffcb86f85a0a63c05c5d36be040f079
Tom Sheffler <thomas.sheffler@weemo.com>
2015-01-15 15:37:55 -0800

    optimize SQL

--- app/controllers/call_controller.rb
+++ app/controllers/call_controller.rb
@@ -2,13 +2,13 @@ class CallController < ApplicationController
 
   def index
     @me = current_user
-    @friends = User.all.select{ |u| u != @me }
+    @friends = User.where('id <> ?', @me.id).order(:name => :asc)
   end
 
   # whose presence we are interested in
   def friends
     @me = current_user
-    @friends = User.all.select{ |u| u != @me }
+    @friends = User.where('id <> ?', @me.id).order(:name => :asc)
 
     # see views/call/friends.json.jbuilder for field selection
     render :format => :json
