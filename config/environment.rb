6ad7b6829dd6f8b924c34142189cb656722e8328
Tom Sheffler <thomas.sheffler@weemo.com>
2015-06-18 10:00:31 -0700

    remove old keys

--- config/environment.rb
+++ config/environment.rb
@@ -6,20 +6,20 @@ env_vars = File.join(Rails.root, '/config/env_vars.rb')
 load(env_vars) if File.exists?(env_vars)
 
 # Password for the admin user
-ADMIN_PASSWORD = ENV['ADMIN_PASSWORD'] || "see#me"
+ADMIN_PASSWORD = ENV['ADMIN_PASSWORD'] || "whatUsee"
 
 # Path to the Weemo CA Cert
-RTCC_CACERT = ENV['RTCC_CACERT']
+# RTCC_CACERT = ENV['RTCC_CACERT']
 
 # Paths to the extracted key and cert from the client.p12 file
-RTCC_CLIENTCERT = ENV['RTCC_CLIENTCERT']
-RTCC_CLIENTCERT_KEY = ENV['RTCC_CLIENTCERT_KEY']
+# RTCC_CLIENTCERT = ENV['RTCC_CLIENTCERT']
+# RTCC_CLIENTCERT_KEY = ENV['RTCC_CLIENTCERT_KEY']
 
 # Password
-RTCC_CERTPASSWORD = ENV['RTCC_CERTPASSWORD']
+# RTCC_CERTPASSWORD = ENV['RTCC_CERTPASSWORD']
 
 # Weemo Auth endpoint, Client ID and Secret
-RTCC_AUTH_URL = ENV['RTCC_AUTH_URL']
+# RTCC_AUTH_URL = ENV['RTCC_AUTH_URL']
 RTCC_CLIENT_ID = ENV['RTCC_CLIENT_ID']
 RTCC_CLIENT_SECRET = ENV['RTCC_CLIENT_SECRET']
 
